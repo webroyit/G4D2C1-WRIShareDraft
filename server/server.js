01562ff224613d202cb91c15e8f6e88dc7116c68
royweb <xytank@gmail.com>
2021-04-23 11:53:14 -0400

    Install socket.io and set it up on the server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+// This is a function
+// Need to fix CORS because client and server port are different
+const io = require('socket.io')(3001, {
+    cors: {
+        // Give permission to access the server
+        origin: 'http://localhost:3000/',
+        methods: ['GET', 'POST']
+    }
+});
+
+// Communication for the client
+io.on('connection', socket => {
+
+})
